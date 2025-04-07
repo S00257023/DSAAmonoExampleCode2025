36228e1928d7b93c152047c0587f164e391ff739
Paul Powell <paul.powell@atu.ie>
2025-04-07 09:22:44 +0100

    Changing DS examples

--- BuiltinDataStructureExamples2025/Program.cs
+++ BuiltinDataStructureExamples2025/Program.cs
@@ -32,17 +32,18 @@
                 XP = 200
             };
             //print<Player>(p);
-            //LL_ex();
+            LL_ex();
             //ST_ex();
             //Queue_ex();
-            Dict_ex();
+            //Dict_ex();
 
             Console.ReadLine();
         }
 
-        static void print<T>(T obj)
+        static void print<T>(T obj) 
         {
-            Console.WriteLine("{0}", obj.ToString());
+            if(obj != null)
+                Console.WriteLine("{0}", obj.ToString());
         }
 
         static void LL_ex()
@@ -50,6 +51,8 @@
             LinkedList<int> someLinkedList = new LinkedList<int>();
             someLinkedList.AddFirst(34);
             someLinkedList.AddFirst(2);
+            //someLinkedList.AddLast(45);
+            someLinkedList.AddBefore(someLinkedList.Last, 23);
             int sum;
             sum = 0;
             Console.WriteLine("First is {0} Last is {1}. ", someLinkedList.First.Value.ToString(), someLinkedList.Last.Value.ToString());
@@ -71,7 +74,8 @@
             stack.Push(1);
             stack.Push(2);
             int i = stack.Pop();
-            Console.WriteLine("Item popped is {0} stack count is {1}. current top item is {2} ", i.ToString(), stack.Count(), stack.Peek());
+            Console.WriteLine("Item popped is {0} stack count is {1}. current top item is {2} ", 
+                                i.ToString(), stack.Count(), stack.Peek());
             Console.ReadKey();
         }
         static void Queue_ex()
@@ -87,24 +91,24 @@
         static void Dict_ex()
         {
             Dictionary<int, Player> players = new Dictionary<int, Player>();
-            players[1] = new Player()
-            {
-                ID = 1,
-                GameTag = "EMO",
-                XP = 400
+            players[42] = new Player()
+            {   
+                ID = 42,GameTag = "EMO", XP = 400
             };
-            players[2] = new Player()
+            players[50] = new Player()
             {
-                ID = 2,
-                GameTag = "Shadow",
-                XP = 200
+                ID = 50, GameTag = "Shadow", XP = 200
             };
+            players.Add(60, new Player()
+            {
+                ID = 60, GameTag = "Sunny", XP = 300
+            });
 
-            Player p = players[1];
+            Player p = players[42];
             print(p);
 
             Player p2;
-            print(players[2]);
+            print(players[50]);
 
             // Printing out Dictionary Keys and Values
             foreach (KeyValuePair<int, Player> kvp in players)
@@ -113,7 +117,7 @@
                 print(kvp.Value);
             }
 
-            if (players.TryGetValue(2, out p2))
+            if (players.TryGetValue(50, out p2))
             {
                 p2.GameTag = "Bob a Job";
                 print(p2);
